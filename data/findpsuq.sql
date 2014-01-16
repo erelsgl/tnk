497cc34ff8595a176bb4d25ecb101bcb5a70a341
erelsgl <erelsgl@gmail.com>
2014-01-16 15:44:45 +0200

    Update data tables

--- data/findpsuq.sql
+++ data/findpsuq.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `findpsuq` (
-  `kotrt_sfr` char(12) CHARACTER SET hebrew DEFAULT NULL,
-  `kotrt` varchar(16) CHARACTER SET hebrew DEFAULT NULL,
-  `ktovt` varchar(16) CHARACTER SET hebrew DEFAULT NULL,
-  `verse_number` int(11) NOT NULL DEFAULT '0',
-  `verse_letter` char(3) CHARACTER SET hebrew DEFAULT NULL,
-  `verse_text` text CHARACTER SET hebrew,
-  `ktovt_trgum` varchar(255) DEFAULT NULL,
-  `ktovt_trgum_xdj` varchar(23) CHARACTER SET hebrew DEFAULT NULL,
-  `ktovt_sikum` varchar(35) CHARACTER SET hebrew DEFAULT NULL
-) ENGINE=InnoDB CHARACTER SET utf8;
+  `kotrt_sfr` char(12) character set hebrew default NULL,
+  `kotrt` varchar(16) character set hebrew default NULL,
+  `ktovt` varchar(16) character set hebrew default NULL,
+  `verse_number` int(11) NOT NULL default '0',
+  `verse_letter` char(3) character set hebrew default NULL,
+  `verse_text` text character set hebrew,
+  `ktovt_trgum` varchar(160) default '',
+  `ktovt_trgum_xdj` varchar(23) character set hebrew default NULL,
+  `ktovt_sikum` varbinary(35) default NULL
+) ENGINE=MyISAM CHARACTER SET utf8;
 
 SET character_set_database=utf8;
 
