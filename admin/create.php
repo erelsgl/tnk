afa9821165dbe2c935578adcd6e468c53054cdff
erelsgl <erelsgl@gmail.com>
2014-04-01 22:40:07 +0300

    fix typo

--- admin/create.php
+++ admin/create.php
@@ -142,7 +142,7 @@ or die ("Can't create db_connect_params");
  * Create the database tables based on the data_utf8 folder.
  */
 function create_database_tables() {
-	$configuration_tables = array("psuqim", "psuqim_niqud_milim", "sfrim", "prqim", "trgumim_im_ktovt", "QLT_mftx". "prt_tnk1", "board_tnk1");
+	$configuration_tables = array("psuqim", "psuqim_niqud_milim", "sfrim", "prqim", "trgumim_im_ktovt", "QLT_mftx", "prt_tnk1", "board_tnk1");
 	foreach ($configuration_tables as $table)
 		restore_table($table);	
 }
