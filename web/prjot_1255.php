c7122c71256a7aad0aeb50cca2f3924637ecfc23
erelsgl <erelsgl@gmail.com>
2014-07-23 12:21:36 +0300

    ajax

--- web/prjot_1255.php
+++ web/prjot_1255.php
@@ -8,32 +8,38 @@
 $SCRIPT=realpath(dirname(__FILE__)."/../script");
 global $TNKUrl;
 
+require_once("$SCRIPT/html.php");
+require_once("$SCRIPT/taconite.php");
+$HTML_ENCODING = 'windows-1255';
+$HTML_DIRECTION = 'rtl';
+$HTML_LANGUAGE = 'he';
+
 require_once("$SCRIPT/prjot_1255.php");
 require_once("$SCRIPT/coalesce.php");
 $from_day = coalesce($_GET['from_day'],0);
 $to_day = coalesce($_GET['to_day'],0);
 sql_set_charset('hebrew');
 $format = isset($_GET['format'])? $_GET['format']: 'html';
+$content = prjot($from_day,$to_day);
 if ($format=='taconite') {
-	require_once("$SCRIPT/taconite.php");
-	$HTML_ENCODING = 'windows-1255';
 	print jquery_taconite_header($HTML_ENCODING)."
 	<replaceContent select=\"#whatsnew_prjot\">
-	".prjot($from_day,$to_day)."
+	$content
 	</replaceContent>
 	".jquery_taconite_footer();
+} else if ($format=='html') {
+	print //xhtml_header("פרשת השבוע", "", array("$TNKUrl/tnk1/_themes/klli.css"))."
+	$content
+	//".xhtml_footer()
+	;
 } else {
-	require_once("$SCRIPT/html.php");
-	$HTML_ENCODING = 'windows-1255';
-	$HTML_DIRECTION = 'rtl';
-	$HTML_LANGUAGE = 'he';
 	print xhtml_header("פרשת השבוע", "", array("$TNKUrl/tnk1/_themes/klli.css"))."
 	<!--NiwutElyon0-->
 	<div class='NiwutElyon'>
 	<div class='NiwutElyon'><a href='$TNKUrl/tnk1/'>אתר הניווט בתנ&quot;ך</a>&gt;</div>
 	</div>
 	<!--NiwutElyon1-->
-	".prjot($from_day,$to_day)."
+	$content
 	".xhtml_footer();
 }
 ?>
--- web/prjot_utf8.php
+++ web/prjot_utf8.php
@@ -1,35 +1,44 @@
 <?php
 /** קידוד אחיד
- * @file prjot.php
+ * @file prjot_utf8.php - weekly torah portion calculations קידוד חלונות
  * @author Erel Segal http://tora.us.fm
  * @date 2009-08-09
  */
+
 $SCRIPT=realpath(dirname(__FILE__)."/../script");
+global $TNKUrl;
+
+require_once("$SCRIPT/html.php");
+require_once("$SCRIPT/taconite.php");
+$HTML_ENCODING = 'utf-8';
+$HTML_DIRECTION = 'rtl';
+$HTML_LANGUAGE = 'he';
 
 require_once("$SCRIPT/prjot_utf8.php");
-
-$HTML_ENCODING = 'utf-8';
+require_once("$SCRIPT/coalesce.php");
+$from_day = coalesce($_GET['from_day'],0);
+$to_day = coalesce($_GET['to_day'],0);
+sql_set_charset('utf8');
 $format = isset($_GET['format'])? $_GET['format']: 'html';
+$content = prjot($from_day,$to_day);
 if ($format=='taconite') {
-	require_once(dirname(__FILE__).'/../_script/taconite.php');
 	print jquery_taconite_header($HTML_ENCODING)."
 	<replaceContent select=\"#whatsnew_prjot\">
-	".prjot(time())."
+	$content
 	</replaceContent>
 	".jquery_taconite_footer();
+} else if ($format=='html') {
+	print xhtml_header("פרשת השבוע", "", array("$TNKUrl/tnk1/_themes/klli.css"))."
+	$content
+	".xhtml_footer();
 } else {
-	require_once(dirname(__FILE__).'/../_script/html.php');
-	$HTML_ENCODING = 'utf-8';
-	$HTML_DIRECTION = 'rtl';
-	$HTML_LANGUAGE = 'he';
-	print xhtml_header("פרשת השבוע", "", array("/tnk1/_themes/klli.css"))."
+	print xhtml_header("פרשת השבוע", "", array("$TNKUrl/tnk1/_themes/klli.css"))."
 	<!--NiwutElyon0-->
 	<div class='NiwutElyon'>
-	<div class='NiwutElyon'><a href='index.html'>אתר הניווט בתנ&quot;ך</a>&gt;</div>
+	<div class='NiwutElyon'><a href='$TNKUrl/tnk1/'>אתר הניווט בתנ&quot;ך</a>&gt;</div>
 	</div>
 	<!--NiwutElyon1-->
-	".prjot(time())."
+	$content
 	".xhtml_footer();
 }
-
 ?>
